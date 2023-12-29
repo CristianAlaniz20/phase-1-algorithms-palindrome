1d249887f7bbb19e582e8fa340a6e8ddde46ba22
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-12-28 20:12:26 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedString = word.split("").reverse().join("")
+  return reversedString === word ? true : false;
 }
 
 /* 
   Add your pseudocode here
+  Make isPalindrome take in a string(All lowercase with no special characaters or spaces)
+  Check to see if string is a palindrome(a word spelled the same forwards and backwards)
+  If string is a palindrome, return true
+  Else return false 
 */
 
 /*
   Add written explanation of your solution here
+  The function isPalindrome has one parameter for an argument that 
+  should be a string in lower case, with no spaces or special characters.
+  Inside the code block, I declared a variable, reversedString, using const
+  which had the value of a string that was split into an array, had 
+  the order reversed, and then joined. In the next line it checks if 
+  the reversedString value is equal to the original string passed in. If 
+  it is, it returns true, else it returns false. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +29,11 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("albert"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
